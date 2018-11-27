8296b4fcff5728b49ca2bba7ab227f4e6256c0fb
sis0k0 <stanimira.vlaeva@gmail.com>
2018-11-27 13:16:37 +0200

    add some movies :)

---
+++ movies.js
@@ -0,0 +1,37 @@
+const movies = [
+  {
+    'title': 'Star Wars: Episode I - The Phantom Menace',
+    'poster': 'star_wars_episode_1_poster.png',
+    'hero_image': 'star_wars_episode_1_hero.jpg'
+  },
+
+  {
+    'title': 'Star Wars: Episode II - Attack of the Clones',
+    'poster': 'star_wars_episode_2_poster.png',
+    'hero_image': 'star_wars_episode_2_hero.jpg'
+  },
+
+  {
+    'title': 'Star Wars: Episode III - Revenge of the Sith',
+    'poster': 'star_wars_episode_3_poster.png',
+    'hero_image': 'star_wars_episode_3_hero.jpg'
+  },
+
+  {
+    'title': 'Star Wars: Episode IV - A New Hope',
+    'poster': 'star_wars_episode_4_poster.png',
+    'hero_image': 'star_wars_episode_4_hero.jpg'
+  },
+
+  {
+    'title': 'Star Wars: Episode V - The Empire Strikes Back',
+    'poster': 'star_wars_episode_5_poster.png',
+    'hero_image': 'star_wars_episode_5_hero.jpg'
+  },
+
+  {
+    'title': 'Star Wars: Episode VI - Return of the Jedi',
+    'poster': 'star_wars_episode_6_poster.png',
+    'hero_image': 'star_wars_episode_6_hero.jpg'
+  }
+];
